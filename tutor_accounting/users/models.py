6c00d533f8b2c73262855a7ef1f7c182d31f5cdf
jeniks <fertuv68@gmail.com>
2024-09-24 17:19:26 +0300

    - Настроена базовая структура проекта - Добавлена кастомная модель ПОЛЬЗОВАТЕЛЯ (CustomUser) - Настроены уры, статика и шаблоны

---
+++ tutor_accounting/users/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name="Логин",
+    )
+    email = models.EmailField(
+        max_length=50,
+        unique=True,
+        verbose_name="Почта",
+    )
+    first_name = models.CharField(
+        max_length=50,
+        verbose_name="Ваше имя",
+    )
+    last_name = models.CharField(
+        max_length=50,
+        verbose_name="Ваша фамилия",
+        blank=True,
+        null=True,
+    )
+    payment = models.IntegerField(default=1000)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.username
